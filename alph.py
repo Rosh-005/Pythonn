d13ca8de00927907e786413c581acd55a8c41e95
Rosh-005 <60419233+Rosh-005@users.noreply.github.com>
2020-01-29 11:47:54 +0530

    Alphabet
    Prints alphabet separately and vowels separately

---
+++ alph.py
@@ -0,0 +1,10 @@
+z=['a','b','c','e','i','j','o','p','u','v','w']
+for i in z:
+    if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u'):
+        print("its vowel",i)
+    else:
+        print("its constant",i) 
+
+
+
+
