d4fb3bda90cc4c9f93a3513e8c142043000370c1
Rosh-005 <60419233+Rosh-005@users.noreply.github.com>
2020-01-29 13:53:00 +0530

    Fare calculation

---
+++ billing.py
@@ -0,0 +1,6 @@
+s=raw_input("enter the source address")
+d=raw_input("enter the destination address ")
+if(s=='toranto' and d=='orlando'):
+    print ("$1500")
+elif(s=='venice' and d=='rome'):
+    print ("$1300")
